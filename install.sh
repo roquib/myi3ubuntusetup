588db3aa534c583c3468e9791e80ddbb3af5fa9d
Abdur Roquib Pramanik <roquib01@outlook.com>
2022-02-24 01:13:55 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -32,6 +32,71 @@ sudo apt install software-properties-common apt-transport-https wget
 # Import the Microsoft GPG key
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
 # add repository
+
 sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
 # install vs-code
 sudo apt install code
+
+
+# add php pppa
+echo "adding php ppa ondrej/php"
+sudo add-apt-repository ppa:ondrej/php -y
+sudo apt-get update
+
+echo "installing php"
+sudo apt install -y php curl php-mbstring php-zip php-gd php-json php-curl php-xml php-sqlite3 php-mysql php-pgsql
+
+echo "installing composer"
+curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
+
+echo 'installing mysql server'
+
+sudo apt install -y mysql-server
+
+sudo mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '';FLUSH PRIVILEGES;"
+
+
+echo "installing laravel"
+composer global require laravel/installer
+echo "laravel valet"
+composer global require cpriego/valet-linux
+
+echo "Installing dependency of valet "
+sudo apt-get install network-manager libnss3-tools jq xsel
+valet install
+
+echo "installing phpmyadmin"
+echo "switching to bash"
+
+bash
+
+DATA="$(wget https://www.phpmyadmin.net/home_page/version.txt -q -O-)"
+URL="$(echo $DATA | cut -d ' ' -f 3)"
+VERSION="$(echo $DATA | cut -d ' ' -f 1)"
+wget https://files.phpmyadmin.net/phpMyAdmin/${VERSION}/phpMyAdmin-${VERSION}-all-languages.tar.gz
+
+tar xvf phpMyAdmin-${VERSION}-all-languages.tar.gz
+echo "switching to zsh"
+zsh
+
+mkdir ~/Sites
+mkdir ~/Sites/phpmyadmin
+mv phpMyAdmin-*/ ~/Sites/phpmyadmin
+
+cd ~/Sites && valet park && cd
+
+curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
+
+echo "installing node"
+nvm install 16
+nvm use 16
+nvm alias default 16
+
+npm install -g yarn
+
+echo "installing fzf"
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+
+echo "installing expo"
+npm install -g expo-cli
